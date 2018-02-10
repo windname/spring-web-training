157ae60525c43dee7137b6ba92ad75a430289fb9
Vladimir Grigoriev <vladimir.grigoriev@codefactorygroup.com>
2018-02-10 15:46:07 +0200

    added response handler

---
+++ src/main/java/cf/spring/springweb/Book.java
@@ -0,0 +1,61 @@
+package cf.spring.springweb;
+
+/**
+ * @author vgrigoriev - 2/1/2018
+ * domain object
+ */
+public class Book {
+
+    private int id;
+
+    private String name;
+
+    private int authorId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(int authorId) {
+        this.authorId = authorId;
+    }
+
+    public Book addId(int id) {
+        this.setId(id);
+        return this;
+    }
+
+    public Book addName(String name) {
+        this.setName(name);
+        return this;
+    }
+
+    public Book addAuthor(int authorId) {
+        this.setAuthorId(authorId);
+        return this;
+    }
+
+
+
+
+    @Override
+    public String toString() {
+        return "Book{" + "id=" + id + ", name='" + name + '\'' + ", authorId=" + authorId + '}';
+    }
+}
--- src/main/java/cf/spring/springweb/HelloRestController.java
+++ src/main/java/cf/spring/springweb/HelloRestController.java
@@ -2,20 +2,66 @@ package cf.spring.springweb;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author vgrigoriev - 2/10/2018
  */
 @RestController
+@SessionAttributes("loginTime")
 public class HelloRestController {
 
     @RequestMapping(value = "/hello-rest", method = RequestMethod.GET)
     public ResponseEntity<String> handle(Model model) {
-        model.addAttribute("message", "Hello World!");
-        return ResponseEntity.ok().body("Hello");
+        return ResponseEntity.ok().body("Hello2");
     }
+
+    // process incomming parameters
+
+    @RequestMapping(value = "/ex1/{bookId}", method = RequestMethod.GET)
+    public ResponseEntity<String> ex1(@PathVariable("bookId") String bookId, Model model) {
+        return ResponseEntity.ok().body("book: " + bookId);
+    }
+
+    @RequestMapping(value = "/ex1_1/", method = RequestMethod.POST)
+    public ResponseEntity<String> ex1_1(@RequestParam String bookId, Model model) {
+        return ResponseEntity.ok().body("book: " + bookId);
+    }
+
+    @RequestMapping(value = "/ex1_2/", method = RequestMethod.POST)
+    public ResponseEntity<String> ex1_2(@CookieValue(value = "JSESSIONID", defaultValue = "10.02.2018") String lastVisitTime, Model model) {
+        return ResponseEntity.ok().body("lastVisitTime: " + lastVisitTime);
+    }
+
+    @GetMapping(value = "/ex1_3/")
+    public ResponseEntity<String> ex1_3(@RequestHeader("Accept-Encoding") String encoding) {
+
+        return ResponseEntity.ok().body("header values: " + encoding);
+    }
+
+    @GetMapping(value = "/ex1_4/")
+    public ResponseEntity<String> ex1_4(@RequestParam String loginTime, Model model) {
+        model.addAttribute("loginTime", loginTime + "_" + System.currentTimeMillis());
+        return ResponseEntity.ok().body("loginTime: " + loginTime);
+    }
+
+    @GetMapping(value = "/ex1_4_1/")
+    public ResponseEntity<String> ex1_4_1(@SessionAttribute String loginTime, Model model) {
+        return ResponseEntity.ok().body("loginTime: " + loginTime);
+    }
+
+    @PostMapping(value = "/ex1_5/")
+    public ResponseEntity<String> ex1_5(@RequestBody Book book, Model model) {
+        return ResponseEntity.ok().body("book: " + book);
+    }
+
+    @GetMapping(value = "/ex3_1/")
+    @ResponseBody
+    public Book ex3_1() {
+        return new Book().addName("Peter Pan");
+    }
+
+
+
+
 }
